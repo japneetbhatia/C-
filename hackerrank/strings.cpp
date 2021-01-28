0f8b3dd2b21eb94031dda7f5034b7312d33ef3a7
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-28 10:30:50 -0800

    Create strings.cpp

---
+++ hackerrank/strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string str1,str2;
+    cin>>str1>>str2;
+    cout<<str1.length()<<" "<<str2.length()<<endl;
+    cout<<str1<<str2<<endl;
+    swap(str1[0],str2[0]);
+    cout<<str1<<" "<<str2;  
+    return 0;
+}
