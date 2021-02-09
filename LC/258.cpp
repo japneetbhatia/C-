39a7a896586fcb89096d442afa1eac69c7212275
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-09 22:17:52 +0530

    Update 258.cpp

--- LC/258.cpp
+++ LC/258.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int findans(int n){
-        int r=0;
-        int ans = 0;
-        while(n){
-            r = n%10;
-            ans += r;
-            n /= 10;
-        }
-        return ans;
-    }
     int addDigits(int num) {
-        int t = findans(num);
-        int ans =findans(t);
-        return ans>=0 ? ans : t;
+         return num == 0 ? 0 : 1 + (num - 1) % 9;
     }
 };
+
+// class Solution {
+// public:
+//     int findans(int n){
+//         int r=0;
+//         int ans = 0;
+//         while(n){
+//             r = n%10;
+//             ans += r;
+//             n /= 10;
+//         }
+//         return ans;
+//     }
+//     int addDigits(int num) {
+//         int t = findans(num);
+//         int ans =findans(t);
+//         return ans>=0 ? ans : t;
+//     }
+// };
