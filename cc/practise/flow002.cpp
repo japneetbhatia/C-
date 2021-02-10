7b43f42e7c4e62bae207f6ce09e21e1addc6a42a
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-10 21:33:51 +0530

    Create flow002.cpp

---
+++ cc/practise/flow002.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int t;
+	cin>>t;
+	while(t--){
+	    int n1,n2;
+	    cin>>n1>>n2;
+	    cout<<n1%n2<<endl;    
+	}
+	return 0;
+}
