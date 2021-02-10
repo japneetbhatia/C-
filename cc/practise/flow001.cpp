d43da947fb4579b9f30a4617786152149f6e4733
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-10 21:32:27 +0530

    Create flow001.cpp

---
+++ cc/practise/flow001.cpp
@@ -0,0 +1,22 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int t;
+	cin>>t;
+	while(t){
+	   int n1,n2,sum=0;
+	   cin>>n1;
+	   cin>>n2;
+	   sum = n1+n2;
+	   cout<<sum<<endl;
+	   t--;
+	}
+	
+
+	return 0;
+}
